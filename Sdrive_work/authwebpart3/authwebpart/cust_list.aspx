6b4fc40e51d02343609ec33c7f050aaa8ca4fff7
panchal999 <parth.np5@gmail.com>
2017-10-24 16:35:04 +0530

    search intelegence

---
+++ Sdrive_work/authwebpart3/authwebpart/cust_list.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cust_list.aspx.cs" Inherits="authwebpart.cust_list" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="False">
+                   <Columns>
+                            <asp:TemplateField HeaderText="c_id" >
+                               
+                                <ItemTemplate>
+                              <!--      <asp:Label ID="Label1" runat="server" Text='<%# Bind("c_id") %>'></asp:Label>-->
+                                    <asp:LinkButton ID="LinkButton2" OnClick="LinkButton2_Click"  Text='<%#Eval("c_id") %>'  runat="server">LinkButton</asp:LinkButton>
+                                    </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="c_fname" HeaderText="c_fname"  />
+                            <asp:BoundField DataField="c_mname" HeaderText="c_mname"  />
+                            <asp:BoundField DataField="c_lname" HeaderText="c_lname"  />
+                            <asp:BoundField DataField="city" HeaderText="city"  />
+                            <asp:BoundField DataField="create_date" HeaderText="create_date" />
+                           
+                        </Columns>
+                        
+                    </asp:GridView>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sdrive_work/authwebpart3/authwebpart/login.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace authwebpart
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["suser"]!=null)
+            {
+                
+                Session.Abandon();
+                Session.Remove(Session["suser"].ToString());
+            }
+            if (Request.UrlReferrer != null)
+            {
+                string previousPageUrl = Request.UrlReferrer.AbsoluteUri;
+                string previousPageName = System.IO.Path.GetFileName(Request.UrlReferrer.AbsolutePath);
+                if (previousPageName.Contains("register.aspx"))
+                {
+                    Response.Write("your request is in queue --you will be informed after admin approval");
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string cons = WebConfigurationManager.ConnectionStrings["authuserConnectionString"].ConnectionString;
+            SqlConnection myConnection = new SqlConnection(cons);
+            myConnection.Open();
+
+            string s = TextBox1.Text;
+            if (s != "" && s != null && TextBox2.Text != null && TextBox2.Text!="")
+            {
+
+
+                string selcquery = "SELECT * FROM reg_user where reg_uname='" + s.ToString() + "'";
+                SqlCommand sccmd1 = new SqlCommand(selcquery, myConnection);
+                SqlDataReader myReader;
+                myReader = sccmd1.ExecuteReader();
+
+
+
+                if (myReader != null && myReader.HasRows)
+                {
+
+                    myReader.Read();
+                    string ruser = myReader["reg_uname"].ToString();
+                    string pass = myReader["reg_pass"].ToString();
+
+
+
+
+                    if (Boolean.Parse(myReader["isvalid"].ToString()))
+                    {
+                        if (pass == TextBox2.Text)
+                        {
+                            if (Int32.Parse(myReader["role_id"].ToString()) == 1)
+                            {
+                                Session["suser"] = ruser;
+                                Response.Redirect("verify_user.aspx");
+                            }
+                            else
+                            {
+                                Session["suser"] = ruser;
+                                FormsAuthentication.RedirectFromLoginPage(TextBox1.Text, true);
+                            }
+                        }
+                        else
+                        {
+                            Response.Write("Invalid Credentials...");
+                        }
+                    }
+                    else
+                    {
+                        Response.Write("your request is in queu...");
+                    }
+                }
+                else
+                {
+                    Response.Write("Invalid Credentials...");
+                }
+
+            }
+            else
+            {
+                Response.Write("Invalid Credentials...");
+            }
+            myConnection.Close();
+            /* authuserEntities ae = new authuserEntities();
+             string s = TextBox1.Text.ToString();
+             reg_user ruser = ae.reg_user.Where(r=>r.reg_uname==s ).FirstOrDefault<reg_user>();
+             */
+        }
+    }
+}
